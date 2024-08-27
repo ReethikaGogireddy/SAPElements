0dbd7c5e48039742bb3cabf19c25d150b3d45531
ReethikaGogireddy <gogireddyreethika1@gmail.com>
2024-08-27 15:01:20 +0530

    button added

--- app/basicwebsite/webapp/controller/App.controller.js
+++ app/basicwebsite/webapp/controller/App.controller.js
@@ -27,68 +27,8 @@ sap.ui.define([
 			MessageToast.show("Logo pressed!");
 		},
         onPress: function (evt) {
-			MessageToast.show(evt.getSource().getId() + " Pressed");
+			MessageToast.show("Button is Pressed");
 		},
-        onArrowsPlacementSelect: function (oEvent) {
-			var oCarousel = this.byId("carouselSample"),
-				sSelectedValue = oEvent.getSource().getSelectedButton().getText();
-
-			if (sSelectedValue in CarouselArrowsPlacement) {
-				oCarousel.setArrowsPlacement(sSelectedValue);
-			}
-		},
-
-		onPageIndicatorPlacementSelect: function (oEvent) {
-			var oCarousel = this.byId("carouselSample"),
-				sSelectedValue = oEvent.getSource().getSelectedButton().getText();
-
-			if (sSelectedValue in CarouselPageIndicatorPlacementType) {
-				oCarousel.setPageIndicatorPlacement(sSelectedValue);
-			}
-		},
-
-		onBackgroundDesignSelect: function (oEvent) {
-			var oCarousel = this.byId("carouselSample"),
-				sSelectedValue = oEvent.getSource().getSelectedButton().getText();
-
-			if (sSelectedValue in BackgroundDesign) {
-				oCarousel.setBackgroundDesign(sSelectedValue);
-			}
-		},
-
-		onPageIndicatorBackgroundDesignSelect: function (oEvent) {
-			var oCarousel = this.byId("carouselSample"),
-				sSelectedValue = oEvent.getSource().getSelectedButton().getText();
-
-			if (sSelectedValue in BackgroundDesign) {
-				oCarousel.setPageIndicatorBackgroundDesign(sSelectedValue);
-			}
-		},
-
-		onPageIndicatorBorderDesignSelect: function (oEvent) {
-			var oCarousel = this.byId("carouselSample"),
-				sSelectedValue = oEvent.getSource().getSelectedButton().getText();
-
-			if (sSelectedValue in BorderDesign) {
-				oCarousel.setPageIndicatorBorderDesign(sSelectedValue);
-			}
-		},
-
-		onShowPageIndicatorChange: function (oEvent) {
-			var oCarousel = this.byId("carouselSample");
-			oCarousel.setShowPageIndicator(oEvent.getParameter("state"));
-		},
-
-		onResizeCarouselContainer: function (oEvent) {
-			var iOriginalHeight = 650,
-				iValue = oEvent.getParameter("value"),
-				oCarouselContainer = this.byId("carouselContainer"),
-				iNewHeight = Math.floor(iOriginalHeight * iValue / 100);
-
-			oCarouselContainer.setWidth(iValue + "%");
-			oCarouselContainer.setHeight(iNewHeight + "px");
-		},
-
 		onNumberOfImagesChange: function (oEvent) {
 			this._setNumberOfImagesInCarousel(Number(oEvent.getParameter("value")));
 		},
@@ -113,18 +53,5 @@ sap.ui.define([
 			}
 		},
 
-		onNumberOfPages: function(oEvent) {
-				const sVisiblePageCount = oEvent.getParameter("value");
-
-				this.byId("carouselSample").getCustomLayout()?.setVisiblePagesCount(Number(sVisiblePageCount));
-		},
-
-		OnScrollModeChange: function(oEvent) {
-			const CarouselScrollMode = mobileLibrary.CarouselScrollMode,
-					bViewMode = oEvent.getParameter("state"),
-					sScrollMode = bViewMode ?  CarouselScrollMode.VisiblePages : CarouselScrollMode.SinglePage;
-
-					this.byId("carouselSample").getCustomLayout()?.setScrollMode(sScrollMode);
-		}
     });
 });
